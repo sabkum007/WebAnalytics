9d2b4a4d2fe116aa53d632ea1f476cc937036c5f
sabkum007 <sabkum007@gmail.com>
2015-10-30 03:46:36 -0700

    commiting frersh files

---
+++ Web_Analytics/Controllers/WebAnalyticsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Analytics.Controllers
+{
+    public class WebAnalyticsController : Controller
+    {
+        //
+        // GET: /WebAnalytics/
+
+        public ActionResult Index()
+        {
+            return View("Test");
+        }
+        public ActionResult TestPage()
+        {
+            return View("Test1");
+        }
+
+    }
+}
---
+++ Web_Analytics/Js/test.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $("form#Submit").submit(function () {
+        var count = $(this).data("count") || 0;
+        if (count >= x) {
+            //user clicked more than you wanted to so do something here 
+        }
+
+        $(this).data("count", ++count);
+        alert(1);
+    });
+
+    function save() {
+        alert(1);
+    }
+});
